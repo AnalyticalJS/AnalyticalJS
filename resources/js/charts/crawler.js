358865ad4fa99605d3a5fc8660c33da65f1aa76e
dev <admin@idevgames.co.uk>
2023-03-03 18:20:15 +0000

    Gradient line charts

--- resources/js/charts/crawler.js
+++ resources/js/charts/crawler.js
@@ -8,7 +8,16 @@ new Chart(document.getElementById('crawler'),
         {
             label: 'Crawled Amount',
             data: data.map(row => row.bots),
-            borderColor: '#6600ff',
+            borderColor: function(context) {
+                const chart = context.chart;
+                const {ctx, chartArea} = chart;
+        
+                if (!chartArea) {
+                  // This case happens on initial chart load
+                  return;
+                }
+                return getGradient(ctx, chartArea);
+              },
             backgroundColor: '#bf9af7'
         }
         ]
@@ -17,3 +26,21 @@ new Chart(document.getElementById('crawler'),
         aspectRatio: 1,
     }
 });
+
+let width, height, gradient;
+function getGradient(ctx, chartArea) {
+  const chartWidth = chartArea.right - chartArea.left;
+  const chartHeight = chartArea.bottom - chartArea.top;
+  if (!gradient || width !== chartWidth || height !== chartHeight) {
+    // Create the gradient because this is either the first render
+    // or the size of the chart has changed
+    width = chartWidth;
+    height = chartHeight;
+    gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
+    gradient.addColorStop(0, '#6600ff');
+    gradient.addColorStop(0.5, '#3fa0f0');
+    gradient.addColorStop(1, '#22ddb7');
+  }
+
+  return gradient;
+}
--- resources/js/charts/daily.js
+++ resources/js/charts/daily.js
@@ -8,7 +8,16 @@ new Chart(document.getElementById('daily'),
         {
             label: 'Pages',
             data: data.map(row => row.pages),
-            borderColor: '#22ddb7',
+            borderColor: function(context) {
+                const chart = context.chart;
+                const {ctx, chartArea} = chart;
+        
+                if (!chartArea) {
+                  // This case happens on initial chart load
+                  return;
+                }
+                return getGradient(ctx, chartArea);
+              },
             backgroundColor: '#c2fff3'
         },
         {
@@ -23,3 +32,21 @@ new Chart(document.getElementById('daily'),
         aspectRatio: 1,
     }
 });
+
+let width, height, gradient;
+function getGradient(ctx, chartArea) {
+  const chartWidth = chartArea.right - chartArea.left;
+  const chartHeight = chartArea.bottom - chartArea.top;
+  if (!gradient || width !== chartWidth || height !== chartHeight) {
+    // Create the gradient because this is either the first render
+    // or the size of the chart has changed
+    width = chartWidth;
+    height = chartHeight;
+    gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
+    gradient.addColorStop(0, '#6600ff');
+    gradient.addColorStop(0.5, '#3fa0f0');
+    gradient.addColorStop(1, '#22ddb7');
+  }
+
+  return gradient;
+}
